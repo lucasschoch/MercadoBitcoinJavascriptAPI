8093eabf48aa0e66e63f6b177189eb17793e19eb
Lucas Schoch <lucas.schoch@atitude.com.br>
2018-03-26 16:49:14 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,62 @@
+var MercadoBitcoinTrade = require('mercadoCoin');
+
+//let paramsOrders = {
+//    'coin_pair': 'BRLBTC'
+//};
+
+//MercadoBitcoinTrade.listOrders(paramsOrders);
+
+//paramsSysMessages = {
+//    'coin_pair': 'BRLBTC'
+//}
+
+//MercadoBitcoinTrade.listSysMessages(paramsSysMessages);
+
+//paramsGetOrders = {
+//    'coin_pair': 'BRLBTC',
+//    'order_id': 1
+//}
+
+//MercadoBitcoinTrade.getOrder(paramsGetOrders);
+
+//paramsListOrderBook = {
+//    'coin_pair': 'BRLBTC',
+//    'full': true
+//}
+
+//MercadoBitcoinTrade.listOrderBook(paramsListOrderBook);
+
+//paramsPlaceBuyOrder = {
+//    'coin_pair': 'BRLBTC',
+//    'quantity': 10.0,
+//    'limit_price':52100.00
+//}
+
+//MercadoBitcoinTrade.placeBuyOrder(paramsPlaceBuyOrder);
+
+//paramsPlaceSellOrder = {
+//    'coin_pair': 'BRLBTC',
+//    'quantity': 10.0,
+//    'limit_price':52100.00
+//}
+
+//MercadoBitcoinTrade.placeSellOrder(paramsPlaceSellOrder);
+
+//paramsCancelOrder = {
+//    'coin_pair': 'BRLBTC',
+//    'order_id': 1
+//}
+//MercadoBitcoinTrade.cancelOrder(paramsCancelOrder);
+
+//paramsGetWithdrawal = {
+//    'coin': 'BRL',
+//    'withdrawal_id': 1
+//}
+//MercadoBitcoinTrade.getWithdrawal(paramsGetWithdrawal);
+
+//paramsWithdrawCoin = {
+//    'coin': 'BRL',
+//    'quantity': 50.0,
+//    'account_ref': 1
+//}
+//MercadoBitcoinTrade.withdrawCoin(paramsWithdrawCoin);
